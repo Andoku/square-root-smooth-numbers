f2cd865fa46c1d309f686560f653d67aaec50ac6
Tatuev Anton <tatuev.a.a@yandex.ru>
2019-05-29 20:25:21 +0300

    Добавил рыбу класса для подсчета количества чисел

---
+++ Calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    Calculator();
+    ~Calculator();
+    
+    void start();
+    
+private:
+};
+
+#endif /* CALCULATOR_H */
--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 CFLAGS=-c -Wall -Wextra -O2 -std=c++11
 LDFLAGS=
-SOURCES=main.cpp
+SOURCES=main.cpp Calculator.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
 EXECUTABLE=out
 
